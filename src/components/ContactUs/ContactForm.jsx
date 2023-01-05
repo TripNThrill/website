de853876e8ca8477ae594e3aab1383315f59f220
Aman Verma <av1831.av@gmail.com>
2023-01-05 22:48:08 +0530

    connect contactUs page with EmailJs

--- src/components/ContactUs/ContactForm.jsx
+++ src/components/ContactUs/ContactForm.jsx
@@ -1,13 +1,38 @@
 import React from 'react'
+import emailjs from '@emailjs/browser';
 import './Contact.css'
+import { useState } from 'react';
+
+const Result = () => {
+    return (
+        <p>Your message has been successfully sent. I will contact you soon.</p>
+    )
+}
 
 function ContactForm() {
+
+    const [result, showResult] = useState(false);
+
+    const sendEmail = (e) => {
+        e.preventDefault();
+
+        emailjs.sendForm('service_oocyjre', 'template_96ug4no', e.target, 'XsP8X-6jPep3jAh1G')
+            .then((result) => {
+                console.log(result.text);
+            }, (error) => {
+                console.log(error.text);
+            });
+
+        e.target.reset();
+        showResult(true);
+    };
+
     return (
         <div className="w-full bg-center bg-no-repeat bg-cover contactme" id="contact">
             <div className="w-full pt-20 pb-20 contactOverlay">
                 <div className="container">
                     <div className="flex w-full justify-center items-center form">
-                        <form className="flex flex-col w-full md:flex-row md:w-4/5 justify-between bg-white overflow-hidden rounded-lg" action="" onSubmit="">
+                        <form className="flex flex-col w-full md:flex-row md:w-4/5 justify-between bg-white overflow-hidden rounded-lg" action="" onSubmit={sendEmail}>
                             <div className="w-full md:w-2/5 p-8 formWord">
                                 <h2 className="text-4xl font-bold mb-5">Say Hello!</h2>
                                 <span>Full Name</span>
@@ -30,7 +55,7 @@ function ContactForm() {
                                 <br />
                                 <button>SUBMIT</button>
 
-                                <div className="row">All Done</div>
+                                <div className="row">{result ? <Result /> : null}</div>
                             </div>
                         </form>
                     </div>
